3f00a46e3691076390c5dda7c401595cd207b5a4
xyzenith31 <m.donnyindraprayuda@gmail.com>
2026-01-13 21:56:02 +0700

    Donny Indra

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -6,8 +6,7 @@ import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-route
 
 // Contexts
 import { AuthProvider } from './context/AuthContext';
-import { UserProvider } from './context/UserContext';
-import { CustomerServiceProvider } from './context/CustomerServiceContext';
+// UserContext & CustomerServiceContext DIHAPUS karena merah (file hilang)
 import { AccountProvider } from './context/AccountContext';
 
 import NProgress from 'nprogress';
@@ -16,10 +15,10 @@ import 'nprogress/nprogress.css';
 // Layouts
 import AuthLayout from './layouts/AuthLayout';
 import AdminLayout from './layouts/AdminLayout';
-import TeknisiLayout from './layouts/TeknisiLayout';
+// TeknisiLayout DIHAPUS karena merah
 import UserLayout from './layouts/UserLayout';
 
-// Pages Auth
+// Pages Auth (INI AMAN, TIDAK DIUBAH)
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
 import VerificationPage from './pages/auth/VerificationPage';
@@ -30,25 +29,15 @@ import CompleteRegistrationPage from './pages/auth/CompleteRegistrationPage';
 // Pages Admin
 import DashboardAdmin from './pages/admin/DashboardAdmin'; 
 import ProfileAdmin from './pages/admin/ProfileAdmin';
-import KeluhanPelangganAdmin from './pages/admin/KeluhanPelangganAdmin';
-import ManajemenKantorAdmin from './pages/admin/ManajemenKantorAdmin';
 import ManajemenPengguna from './pages/admin/ManajemenPengguna';
+// KeluhanPelangganAdmin & ManajemenKantorAdmin DIHAPUS karena merah
 
-// Pages Teknisi
-import DashboardTeknisi from './pages/teknisi/DashboardTeknisi';
-import ProfileTeknisi from './pages/teknisi/ProfileTeknisi'; 
-import OrderanServisTeknisi from './pages/teknisi/OrderanServisTeknisi';
-import DetailOrderTeknisi from './pages/teknisi/DetailOrderTeknisi';
-import RiwayatTeknisi from './pages/teknisi/RiwayatTeknisi';
-import CustomerServiceTeknisi from './pages/teknisi/CustomerServiceTeknisi';
+// Pages Teknisi (SEMUA DIHAPUS karena merah/fitur dihapus)
 
 // Pages User
 import BerandaUser from './pages/users/BerandaUser';
-import RiwayatUser from './pages/users/RiwayatUser';
 import ProfileUser from './pages/users/ProfileUser';
-import LayananServisUser from './pages/users/LayananServisUser';
-import DetailOrderUser from './pages/users/DetailOrderUser';
-import CustomerServiceUser from './pages/users/CustomerServiceUser'; 
+// Riwayat, Layanan, DetailOrder, CustomerService DIHAPUS karena merah
 
 // Pages 404
 import NotFoundNavbar from './pages/404/404Navbar';
@@ -82,58 +71,45 @@ const App = () => {
     return (
         <BrowserRouter>
             <AuthProvider>
-                <UserProvider>
-                    <CustomerServiceProvider>
-                        <AccountProvider> 
-                            <PageWrapper />
-                            <Routes>
-                                
-                                <Route element={<AuthLayout />}>
-                                    <Route path="/login" element={<LoginPage />} />
-                                    <Route path="/register" element={<RegisterPage />} />
-                                    <Route path="/verification" element={<VerificationPage />} />
-                                    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-                                    <Route path="/reset-password" element={<ResetPasswordPage />} />
-                                    <Route path="/complete-registration" element={<CompleteRegistrationPage />} />
-                                    <Route path="/auth-error" element={<NotFoundAuth />} />
-                                </Route>
-                                
-                                <Route path="/admin" element={<AdminLayout />}>
-                                    <Route path="dashboard" element={<DashboardAdmin />} />
-                                    <Route path="profile" element={<ProfileAdmin />} />
-                                    <Route path="users" element={<ManajemenPengguna />} /> 
-                                    <Route path="customer-service" element={<KeluhanPelangganAdmin />} />
-                                    <Route path="offices" element={<ManajemenKantorAdmin />} />
-                                    <Route index element={<Navigate to="/admin/dashboard" />} />
-                                    <Route path="*" element={<NotFoundSidebar homeUrl="/admin/dashboard" roleName="Admin Dashboard" />} />
-                                </Route>
+                {/* UserProvider & CustomerServiceProvider DIHAPUS dari sini */}
+                <AccountProvider> 
+                    <PageWrapper />
+                    <Routes>
+                        
+                        {/* --- AUTH ROUTES (TETAP UTUH) --- */}
+                        <Route element={<AuthLayout />}>
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route path="/register" element={<RegisterPage />} />
+                            <Route path="/verification" element={<VerificationPage />} />
+                            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+                            <Route path="/reset-password" element={<ResetPasswordPage />} />
+                            <Route path="/complete-registration" element={<CompleteRegistrationPage />} />
+                            <Route path="/auth-error" element={<NotFoundAuth />} />
+                        </Route>
+                        
+                        {/* --- ADMIN ROUTES --- */}
+                        <Route path="/admin" element={<AdminLayout />}>
+                            <Route path="dashboard" element={<DashboardAdmin />} />
+                            <Route path="profile" element={<ProfileAdmin />} />
+                            <Route path="users" element={<ManajemenPengguna />} /> 
+                            {/* Route Customer Service & Offices DIHAPUS */}
+                            <Route index element={<Navigate to="/admin/dashboard" />} />
+                            <Route path="*" element={<NotFoundSidebar homeUrl="/admin/dashboard" roleName="Admin Dashboard" />} />
+                        </Route>
 
-                                <Route path="/teknisi" element={<TeknisiLayout />}>
-                                    <Route path="dashboard" element={<DashboardTeknisi />} />
-                                    <Route path="profile" element={<ProfileTeknisi />} />
-                                    <Route path="jobs" element={<OrderanServisTeknisi />} />
-                                    <Route path="history" element={<RiwayatTeknisi />} />
-                                    <Route path="orders/:id" element={<DetailOrderTeknisi />} />
-                                    <Route path="customer-service" element={<CustomerServiceTeknisi />} />
-                                    <Route index element={<Navigate to="/teknisi/dashboard" />} />
-                                    <Route path="*" element={<NotFoundSidebar homeUrl="/teknisi/dashboard" roleName="Teknisi Dashboard" />} />
-                                </Route>
+                        {/* --- TEKNISI ROUTES (FULL HAPUS) --- */}
 
-                                <Route path="/" element={<UserLayout />}>
-                                    <Route path="dashboard" element={<BerandaUser />} />
-                                    <Route path="profile" element={<ProfileUser />} />
-                                    <Route path="services" element={<LayananServisUser />} />
-                                    <Route path="history" element={<RiwayatUser />} />
-                                    <Route path="orders/:id" element={<DetailOrderUser />} />
-                                    <Route path="customer-service" element={<CustomerServiceUser />} />
-                                    <Route index element={<Navigate to="/dashboard" />} />
-                                    <Route path="*" element={<NotFoundNavbar />} />
-                                </Route>
-                                
-                            </Routes>
-                        </AccountProvider>
-                    </CustomerServiceProvider>
-                </UserProvider>
+                        {/* --- USER ROUTES --- */}
+                        <Route path="/" element={<UserLayout />}>
+                            <Route path="dashboard" element={<BerandaUser />} />
+                            <Route path="profile" element={<ProfileUser />} />
+                            {/* Route Services, History, Orders, CS DIHAPUS */}
+                            <Route index element={<Navigate to="/dashboard" />} />
+                            <Route path="*" element={<NotFoundNavbar />} />
+                        </Route>
+                        
+                    </Routes>
+                </AccountProvider>
             </AuthProvider>
         </BrowserRouter>
     );
