0e18374f25ff00c48ee93b1653ccb9a3a18df3c8
xyzenith31 <m.donnyindraprayuda@gmail.com>
2026-01-14 20:40:53 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,19 +17,23 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'username' => 'testuser',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
-            'role' => 'user',
+            'name' => 'Moch Donny Indra Prayuda',
+            'username' => 'admin_donny',
+            'email' => 'mochdonnyindraprayuda@gmail.com',
+            'phone_number' => '085198342115',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'email_verified_at' => now(),
         ]);
-        
+
         User::factory()->create([
-            'name' => 'Teknisi Handal',
-            'username' => 'teknisi1',
-            'email' => 'teknisi@example.com',
-            'password' => bcrypt('password'),
-            'role' => 'teknisi',
+            'name' => 'Donny Teknisi',
+            'username' => 'teknisi_donny',
+            'email' => 'm.donnyindraprayuda@gmail.com',
+            'phone_number' => '085194928331',
+            'password' => Hash::make('teknisi123'),
+            'role' => 'user',
+            'email_verified_at' => now(),
         ]);
     }
 }
